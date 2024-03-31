3ff423f2ce8de9795ec2450f1cfdecb022e71fd4
nikita9575 <nikita28231997@gmail.com>
2024-03-31 14:48:16 +0530

    Initial Commit

---
+++ manishaProjects/manishaProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  manishaProjectsApp.swift
+//  manishaProjects
+//
+//  Created by nikita on 31/03/24.
+//
+
+import SwiftUI
+
+@main
+struct manishaProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
